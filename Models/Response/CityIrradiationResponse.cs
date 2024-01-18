18e5d25841231868247b4aea997e93d8671b2746
rodolphostoppan <rodolpho.toppan@db1.com.br>
2024-01-18 12:12:49 -0300

    changes that i don't know why

--- Models/Response/CityIrradiationResponse.cs
+++ Models/Response/CityIrradiationResponse.cs
@@ -1,19 +1,10 @@
-using Microsoft.AspNetCore.Mvc;
-
 namespace IrradApi.Models.Response;
 
-public class CityIrradiationResponse : IActionResult
+public class CityIrradiationResponse
 {
     public int StatusCode { get; set; } = 200;
-    public CityIrradiationDataResponse? CityIrradiationData { get; set; }
+    public List<CityIrradiationDataResponse>? CityIrradiationData { get; set; }
 
     public void SetStatusCode(int statusCode) => StatusCode = statusCode;
-
-    public async Task ExecuteResultAsync(ActionContext context)
-    {
-        var result = new ObjectResult(this) { StatusCode = StatusCode };
-
-        await result.ExecuteResultAsync(context);
-    }
 }
 
